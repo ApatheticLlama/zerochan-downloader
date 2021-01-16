ce1a9b96f857a76e297ef5bfa32b16924d484e77
enyoukai <52297896+enyoukai@users.noreply.github.com>
2021-01-16 15:25:27 -0800

    Update linkgenerator.py

--- linkgenerator.py
+++ linkgenerator.py
@@ -1,21 +1,14 @@
 class LinkGenerator():
-    def __init__(self, keywords, sort = "popular"):
+    def __init__(self, keywords, count, sort):
         self.keywords = keywords
+        self.count = count
         self.sort = sort
     
-    def get_keywords(self):
-        return self.keywords
-
-    def get_sort(self):
-        return self.sort
-    
-    def generate_base_link(self):
-        return "https://zerochan.net/" + ', '.join(self.keywords) + "?s=" 
-    
-    def generate_useful_link(self, base_link):
+    def generate_link(self):
         switcher = {"recent": "id", "popular": "fav", "random": "random"}
         sort_for_link = switcher[self.sort]
-        return base_link + sort_for_link
+        return "https://zerochan.net/" + ', '.join(self.keywords) + "?s=" + sort_for_link
+
 
 #test_link = LinkGenerator("Kemonomimi", "popular")
-#print(test_link.generate_useful_link(test_link.generate_base_link()))
+#print(test_link.generate_link())
