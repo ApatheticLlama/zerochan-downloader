910b268f3447141926cf9bbe57f3cd8b0425432f
enyoukai <52297896+enyoukai@users.noreply.github.com>
2021-01-16 15:24:38 -0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import os
 import tkinter as tk
 from tkinter import filedialog, OptionMenu
 
+from linkgenerator import LinkGenerator
 from pagereader import PageReader
 
 class DownloaderGUI:
@@ -12,20 +13,30 @@ class DownloaderGUI:
         self.root = tk.Tk()
         self.root.title("Zerochan Downloader v0.0.1")
         self.option_list = ("Popular", "Recent", "Random")
-        self.v = tk.StringVar()
-        self.v.set("Popular")
+
+        self.default_sort = tk.StringVar()
+        self.default_sort.set("Popular")
+
+        self.download_dir = tk.StringVar()
+        self.download_dir.set(os.getcwd())
+
+        self.frm_dir = tk.Frame()
+        self.frm_download = tk.Frame()
 
         self.lbl_tags = tk.Label(text="Tags (Comma Separated)")
-        self.lbl_count = tk.Label(text="Number of Pages to Parse")
+        self.lbl_count = tk.Label(text="Number of images")
         self.lbl_sort = tk.Label(text="Sort")
 
         self.ent_tags = tk.Entry()
         self.ent_count = tk.Entry()
 
-        self.opt_sort = tk.OptionMenu(self.root, self.v, *self.option_list) 
+        self.opt_sort = tk.OptionMenu(self.root, self.default_sort, *self.option_list) 
 
-        self.btn_dir = tk.Button(text="Folder", command=self.get_dir)
-        self.btn_download = tk.Button(text="Download", command=self.download_images)
+        self.btn_dir = tk.Button(text="Folder", command=self.get_dir, master=self.frm_dir)
+        self.btn_dir.pack()
+
+        self.btn_download = tk.Button(text="Download", command=self.download_images, master=self.frm_download)
+        self.btn_download.pack()
 
         # i don't know how to do this in any better way
         self.lbl_tags.pack()
@@ -37,15 +48,23 @@ class DownloaderGUI:
         self.lbl_sort.pack()
         self.opt_sort.pack()
 
-        self.btn_dir.pack()
-        self.btn_download.pack()
+        self.frm_dir.pack()
+        self.frm_download.pack()
+
 
     def get_dir(self):
         self.download_dir = filedialog.askdirectory()
 
+    def get_link(self):
+        
+        url = LinkGenerator(self.ent_tags.get(), self.ent_count.get(), self.opt_sort.get())
+        link = url.generate_link()
+        return link
+    
     def download_images(self):
         pass
 
 if __name__ == "__main__":
     bruh = DownloaderGUI()
     tk.mainloop()
+    print(bruh.get_link())
